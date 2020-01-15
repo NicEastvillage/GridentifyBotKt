780980376022ed0fc7d7e1a564e444cbc0310069
NicEastvillage <nicoesterby@gmail.com>
2020-01-15 12:25:37 +0100

    Separated HytakBoard bots to a func.

--- src/main/kotlin/east/gridentify/Gridentify.kt
+++ src/main/kotlin/east/gridentify/Gridentify.kt
@@ -36,6 +36,37 @@ fun runManyBots() {
     println("Average time: ${formatTime(avgMillis)}")
 }
 
+@ExperimentalStdlibApi
+fun rubHytakServerBots() {
+    val bots = 5
+
+    val results = mutableListOf<GridentifyBot.Result>()
+
+    for (i in 0 until bots) {
+        HytakServerBoard("East", "35.193.192.221", 32123).use { hytakBoard ->
+            val bot = GridentifyBot(hytakBoard, depth = 2)
+            bot.start { board, cachedMoves ->
+                val moves = cachedMoves ?: findAllMoves(board)
+                8 * moves.size.toDouble() + board.tiles.flatten().zip(weights).sumBy { (tile, w) -> tile.avgValue() * w }
+                //8 * findNeighbourPairsSum(board) + board.tiles.flatten().zip(weights).sumBy { (tile, w) -> tile.avgValue() * w }.toDouble()
+            }.also {
+                val (board, millis) = it
+                results.add(it)
+                println("Bot ${results.size}/$bots | #${i + 1} ::: score: ${board.scoreMin}, time: ${formatTime(millis)}")
+            }
+        }
+    }
+
+    val bestScore = results.maxBy { (board, _) -> board.scoreMin }!!.finalBoard.scoreMin
+    val avgScore = results.map { (board, _) -> board.scoreMin }.average()
+    val avgMillis = results.map { (_, millis) -> millis }.average().toLong()
+
+    println("\n${results.size} runs done!")
+    println("Best score: $bestScore")
+    println("Average score: $avgScore")
+    println("Average time: ${formatTime(avgMillis)}")
+}
+
 val weights = arrayOf(
         arrayOf(4, 3, 1, 3, 4),
         arrayOf(3, 2, 1, 2, 3),
@@ -46,15 +77,5 @@ val weights = arrayOf(
 
 @ExperimentalStdlibApi
 fun main() {
-    for (x in 0..1) {
-        HytakServerBoard("East", "35.193.192.221", 32123).use { hytakBoard ->
-            val bot = GridentifyBot(hytakBoard, depth = 2)
-            bot.start { board, cachedMoves ->
-                //val moves = cachedMoves ?: findAllMoves(board)
-                //moves.size.toDouble() + board.tiles.flatten().zip(weights).sumBy { (tile, w) -> tile.avgValue() * w }
-                8 * findNeighbourPairsSum(board) + board.tiles.flatten().zip(weights).sumBy { (tile, w) -> tile.avgValue() * w }.toDouble()
-            }
-        }
-        Thread.sleep(20)
-    }
+    rubHytakServerBots()
 }
